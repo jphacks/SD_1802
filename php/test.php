6cba2c2cc868c5a43814acff61c8dc886bf7ebf4
teriyaki398 <teriyakiisacookery@gmail.com>
2018-10-20 17:42:25 +0900

    execute python from php

--- php/test.php
+++ php/test.php
@@ -1,12 +1,14 @@
 <?php
-
+    $file_path = "python ~/";
     //一次ファイルができているか
     if(is_uploaded_file($_FILES['up_file']['tmp_name'])){
 
         //一次ファイルをコピーして保存
         if(move_uploaded_file($_FILES['up_file']['tmp_name'],"./upload/".$_FILES['up_file']['name'])){
             //正常
-            echo "uploaded";
+          echo "uploaded";
+          exec($file_path, $output);
+          var_dump($output[0]);
 
         }else{
             //コピーに失敗
