7fc272ac141d3355cc7f57f85e0c9c94fea61f1c
teriyaki398 <teriyakiisacookery@gmail.com>
2018-10-20 16:09:31 +0900

    add php

--- products/config/routes.rb
+++ products/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   get    'users',  to: 'users#new'
   post   'users',  to: 'users#create'
+  post   'bin',    to: 'hello.php'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
